e7aaa4f2e0e5869a082b9317bd8e68094aa39322
mnaibei <muchajulius@gmail.com>
2023-03-30 21:09:30 +0300

    Made requested changes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const tasksContainer = document.querySelector('.displayTasks');
 const inputField = document.querySelector('.taskInput');
 const formField = document.querySelector('#task');
 const clearBtn = document.querySelector('.clear-task');
+const deleteBtn = document.querySelector('.displayTasks');
 
 // getting tasks from local storage
 let tasks = getTasksFromStorage();
@@ -29,4 +30,15 @@ clearBtn.addEventListener('click', () => {
   tasks = clearCompletedTasks(tasks);
   setTasksToStorage(tasks);
   displayTasks(tasks, tasksContainer);
+});
+
+deleteBtn.addEventListener('click', (e) => {
+  if (e.target.classList.contains('del')) {
+    tasks = tasks.filter((task) => task.id !== Number(e.target.dataset.index));
+    tasks = tasks.map((task, id) => ({
+      ...task, id: id + 1,
+    }));
+    setTasksToStorage(tasks);
+    displayTasks(tasks, tasksContainer);
+  }
 });
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,12 +1,14 @@
-import { toggle, inlineEditTask } from './tasks.js';
-import { setTasksToStorage } from './storage.js';
+import { toggle } from './tasks.js';
+import { setTasksToStorage, getTasksFromStorage } from './storage.js';
 
 /* eslint-disable import/prefer-default-export */
 export const displayTasks = (tasks, container) => {
   container.innerHTML = '';
   tasks.forEach((task) => {
     const li = document.createElement('li');
-    li.innerHTML = `<input type="checkbox" class="checkbox ${task.completed ? 'completed' : ''}" id="task-${task.id}" ${task.completed ? 'checked' : ''}><label for="task-${task.id}">${task.desc}</label>`;
+    li.innerHTML = `<input type="checkbox" class="checkbox ${task.completed ? 'completed' : ''}" id="task-${task.id}" ${task.completed ? 'checked' : ''}>
+    <input class="description" id="${task.id}" value="${task.desc}"></input>
+    <button class="del" data-index="${task.id}">Delete</button>`;
     const liInput = li.querySelector('input[type="checkbox"]');
     liInput.addEventListener('click', () => {
       toggle(tasks, task.id);
@@ -15,18 +17,18 @@ export const displayTasks = (tasks, container) => {
     });
 
     // edit task description
-    const liLabel = li.querySelector('label');
-    liLabel.setAttribute('contenteditable', 'true');
-    liLabel.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        liLabel.blur();
-      }
-    });
-    liLabel.addEventListener('blur', () => {
-      const newDesc = liLabel.innerText;
-      inlineEditTask(tasks, task.id, newDesc);
-      setTasksToStorage();
+    const liLabel = li.querySelector('.description');
+
+    liLabel.addEventListener('change', (e) => {
+      let tasks = getTasksFromStorage();
+      tasks = tasks.map((task, i) => {
+        const temp = {};
+        temp.desc = e.target.id - 1 === i ? e.target.value : task.desc;
+        temp.completed = task.completed;
+        temp.id = task.id;
+        return temp;
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasks));
     });
 
     const separator = document.createElement('hr');
